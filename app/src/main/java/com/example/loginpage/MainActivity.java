14e8d6bbe04f3ed6d87eba1268c11ba1b7f9ec92
Jkites <carblue1@gmail.com>
2022-03-05 22:47:55 -0800

    added calendar

--- app/src/main/java/com/example/loginpage/MainActivity.java
+++ app/src/main/java/com/example/loginpage/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.loginpage;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -23,13 +26,10 @@ public class MainActivity extends AppCompatActivity {
     private void correctCredentials(View view){
         EditText username = findViewById(R.id.editTextTextPersonName);
         EditText password = findViewById(R.id.editTextTextPassword);
-        if (username.getText().toString().equals("Baer")){
-            if (password.getText().toString().equals("hello1234")){
-                openActivity2();
-            }
-            else{
-                Snackbar.make(view, "Invalid Credentials", Snackbar.LENGTH_SHORT).show();
-            }
+        InputMethodManager hide_keyboard = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        hide_keyboard.hideSoftInputFromWindow(view.getWindowToken(),0);
+        if (username.getText().toString().equals("Baer")&&password.getText().toString().equals("hello1234")) {
+            openActivity2();
         }
         else {
             Snackbar.make(view, "Invalid Credentials", Snackbar.LENGTH_SHORT).show();
--- app/src/main/java/com/example/loginpage/MainActivity2.java
+++ app/src/main/java/com/example/loginpage/MainActivity2.java
@@ -1,13 +1,23 @@
 package com.example.loginpage;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.TextView;
 
 import com.google.android.material.snackbar.Snackbar;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+
 public class MainActivity2 extends AppCompatActivity {
+
+    CalendarView calendarView;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +26,25 @@ public class MainActivity2 extends AppCompatActivity {
 
         View activity2_layout = findViewById(R.id.activity_main2);
         Snackbar.make(activity2_layout, "Logged In", Snackbar.LENGTH_SHORT).show();
+
+        calendarView = findViewById(R.id.calendarView);
+        button = findViewById(R.id.date_button);
+
+        //updateDate(time.get); how to get current date
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int year, int month, int day) {
+                updateDate(year,month,day);
+            }
+        });
+    }
+    private String getMonthName(int month_num){
+        String[] months={"January","February", "March", "April", "May","June","July",
+                "August","September","October","November","December"};
+        return months[month_num];
+    }
+    private void updateDate(int year, int month, int day){
+            String date_str = "Edit "+getMonthName(month)+" "+day+", "+year;
+            button.setText(date_str);
     }
 }
