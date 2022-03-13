c3abbeee46505043cc43d538210b7f3e0ecbc227
Jkites <carblue1@gmail.com>
2022-03-13 15:41:32 -0700

    added editing functionality and login image

--- app/src/main/java/com/example/loginpage/JournalPage.java
+++ app/src/main/java/com/example/loginpage/JournalPage.java
@@ -39,9 +39,23 @@ public class JournalPage extends AppCompatActivity {
         description = findViewById(R.id.editTextDescription);
         saveButton = findViewById(R.id.saveButton);
 
+        if (date_exists()){
+            fillFields(rating, description);
+        }
+
         TextView date_text = findViewById(R.id.journalDate);
         date_text.setText(current_day.toString());
         saveButton.setOnClickListener(view -> saveText(view));
+    }
+    //populate text fields with previous information
+    private void fillFields(EditText rating, EditText description){
+        for (int index_num=0; index_num<dates.size();index_num++) {
+            if (dates.get(index_num).month == current_day.month && dates.get(index_num).year == current_day.year &&
+                    dates.get(index_num).day == current_day.day) {//checks if month, year, and day number are equal
+                rating.setText(ratings.get(index_num));
+                description.setText(descriptions.get(index_num));
+            }
+        }
     }
     //check if rating is valid integer
     private boolean is_int(String unknown_integer){
@@ -67,17 +81,25 @@ public class JournalPage extends AppCompatActivity {
     //on click of saveButton
     private void saveText(View view){
         String input_rating = rating.getText().toString();
-        String input_description = rating.getText().toString();
+        String input_description = description.getText().toString();
        if (is_int(input_rating)&&Integer.parseInt(input_rating)<=5&&Integer.parseInt(input_rating)>0) {
            if (!input_description.isEmpty()||input_description!=null){
-               if (!date_exists()){ //date does not exist
+               if (!date_exists()){
                    ratings.add(input_rating);
                    descriptions.add(input_description);
                    dates.add(current_day.newInstance(current_day));
+                   Snackbar.make(view, "Entry successfully saved", Snackbar.LENGTH_SHORT).show();
                    //dates.add(current_day); realize that objects, like arrays are accessed through memory, so simply adding it like this will cause it to change whenever the original object changes
                    Log.i("updateArray",current_day.toString());
                } else {
-                   Snackbar.make(view, "Date already exists!", Snackbar.LENGTH_SHORT).show();
+                   for (int index_num=0; index_num<dates.size();index_num++) {
+                       if (dates.get(index_num).month == current_day.month && dates.get(index_num).year == current_day.year &&
+                               dates.get(index_num).day == current_day.day) {
+                           ratings.set(index_num, input_rating);
+                           descriptions.set(index_num,input_description);
+                           Snackbar.make(view, "Entry overwritten", Snackbar.LENGTH_SHORT).show();
+                       }
+                   }
                }
            } else {
                Snackbar.make(view, "Error in description text", Snackbar.LENGTH_SHORT).show();
